b4770022a929370171f66799a6d1e8359695ca91
nawapans <nawapan.suntorachai@students.iaac.net>
2021-01-18 18:29:21 +0700

    change

--- assignment 1.2/script.js
+++ assignment 1.2/script.js
@@ -1,23 +1,25 @@
 (function() {
-    
+
     var mX, mY, distance,
         $distance = $('#distance span');
-        $element  = $('#element');
+    $element = $('#element');
+
+
+    $(document).mousemove(function(event) {
+        $("span ").text("" + event.pageX + "," + event.pageY);
+    });
 
     function calculateDistance(elem, mouseX, mouseY) {
-        return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+        return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left + (elem.width() / 2)), 2) + Math.pow(mouseY - (elem.offset().top + (elem.height() / 2)), 2)));
     }
 
 
-    $(document).mousemove(function(event){
-        $("span").text("X: " + event.pageX + ", Y: " + event.pageY);
-      });
-
-    $(document).mousemove(function(e) {  
+    $(document).mousemove(function(e) {
         mX = e.pageX;
         mY = e.pageY;
         distance = calculateDistance($element, mX, mY);
-        $distance.text(distance);         
+        $distance.text(distance);
     });
+
 
 })();
